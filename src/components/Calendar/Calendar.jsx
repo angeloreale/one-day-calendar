8a544616c398edc5d0b11f3fea63856bdaa335bb
Angelo Reale <angeloreale@pm.me>
2020-10-24 16:31:52 -0300

    Initial sample data visualization works as expected.

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import _ from 'lodash';
+import {
+  START_TIME, END_TIME, EVENTS, INTERVAL,
+} from './constants';
+
+import './Calendar.scss';
+
+const parseHourAMPM = (hour) => (hour > 12 ? hour - 12 : hour);
+
+const renderHourTimestamp = (start, id) => (
+  <div className="M-HourLabel" key={`timestamp--${id}`}>
+    <div className="M-HourLabel__full-hour">
+      {parseHourAMPM(start)}
+      :00
+      <span className="M-HourLabel__full-hour--ampm">
+        {start < 12 ? 'am' : 'pm'}
+      </span>
+    </div>
+    <div className="M-HourLabel__half-hour">
+      {parseHourAMPM(start)}
+      :30
+    </div>
+  </div>
+);
+
+const Calendar = () => {
+  const numHours = END_TIME - START_TIME;
+
+  const eventsWithOffset = _.map(EVENTS,
+    (ev) => ({ start: ev.start, end: ev.end, offset: 0 }));
+
+  const evaluateEvent = (position, events) => {
+    let counter = 0;
+    _.forEach(events, (_event, i) => {
+      if (i !== position) {
+        if ((_event.start >= events[position].start && _event.start < events[position].end)
+          || (events[position].start < _event.end && events[position].start >= _event.start)) {
+          counter += 1;
+          eventsWithOffset[i].offset = eventsWithOffset[position].offset + 1;
+        }
+      }
+    });
+    return { numCols: counter + 1, offset: eventsWithOffset[position].offset };
+  };
+  return (
+    <div className="O-Calendar">
+      <div className="O-Calendar__left-timestamps">
+        {_.times(numHours, (_i) => renderHourTimestamp(_i + START_TIME, _i))}
+        <div className="M-HourLabel--last">
+          <div className="M-HourLabel__full-hour M-HourLabel__full-hour--last">
+            {parseHourAMPM(END_TIME)}
+            :00
+            <span className="M-HourLabel__full-hour--ampm">
+              {END_TIME < 12 ? 'am' : 'pm'}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="O-Calendar__right-events-grid">
+        {_.times(numHours, (_i) => (
+          <div key={`timeblock--${_i}`} className="M-TimeBlock" />
+        ))}
+        <div className="M-EventsGrid--wrapper">
+          <div className="M-EventsGrid">
+            {_.map(EVENTS, (event, position) => {
+              const { numCols, offset } = evaluateEvent(position, eventsWithOffset);
+              const width = 100 / (numCols);
+              const height = 48 * ((event.end - event.start) / 30) * INTERVAL;
+              const top = 48 * (event.start / 30) * INTERVAL;
+              const left = width * (offset);
+              const isHalfHour = event.end - event.start === 30;
+              return (
+                <div
+                  style={{
+                    width: `${width}%`, height, top, left: `${left}%`,
+                  }}
+                  className="M-Event--wrapper"
+                >
+                  <div className={`M-Event ${isHalfHour ? 'M-Event--half' : ''}`}>
+                    Lorem ipsum dolor sit amet
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Calendar;
---
+++ src/components/Calendar/constants.js
@@ -0,0 +1,11 @@
+export const EVENTS = [
+  { start: 30, end: 150 },
+  { start: 180, end: 240 },
+  { start: 180, end: 240 },
+  { start: 300, end: 360 },
+  { start: 330, end: 390 },
+  { start: 390, end: 420 },
+];
+export const START_TIME = 8;
+export const END_TIME = 19;
+export const INTERVAL = 0.5;
