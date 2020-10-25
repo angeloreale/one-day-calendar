c8eea748271602134618d16114fee14981e64317
Angelo Reale <angeloreale@pm.me>
2020-10-25 16:05:00 -0300

    Fix build

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -139,6 +139,7 @@ module.exports = function (webpackEnv) {
           options: {
             sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment,
             root: paths.appSrc,
+            removeCR: true,
           },
         },
         {
